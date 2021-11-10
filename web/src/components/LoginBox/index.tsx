f5bcb2d75d84e9432096d83f42a9bb0f76fa0f78
Diego Luis <contato.diegoluisdasilva@gmail.com>
2021-11-10 19:53:23 -0300

    Criação do login e envio de mensagens

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,52 +1,11 @@
-import { useEffect } from "react";
-
 import styles from "./styles.module.scss";
-
 import { VscGithubInverted } from "react-icons/vsc";
 
-import { api } from "../../services/api";
-
-type AuthResponse = {
-  token: string;
-  user: {
-    id: string;
-    avatar_url: string;
-    name: string;
-    login: string;
-  };
-};
+import { AuthContext } from "../../contexts/auth";
+import { useContext } from "react";
 
 export function LoginBox() {
-  // Sem o redirect configurado no github
-  // const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=${}&redirect_uri=http://localhost:3000`
-
-  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=${"70e16479b18e4ea349cd"}`;
-
-  async function signIn(githubCode: string) {
-    const response = await api.post<AuthResponse>("authenticate", {
-      code: githubCode,
-    });
-
-    const { token, user } = response.data;
-
-    localStorage.setItem("@dowhile:token", token);
-
-    console.log(user);
-  }
-
-  useEffect(() => {
-    const url = window.location.href;
-    const hasGithubCode = url.includes("?code=");
-
-    if (hasGithubCode) {
-      const [urlWithoutCode, githubCode] = url.split("?code=");
-      console.log({ urlWithoutCode, githubCode });
-
-      window.history.pushState({}, "", urlWithoutCode);
-
-      signIn(githubCode);
-    }
-  }, []);
+  const { signInUrl } = useContext(AuthContext);
 
   return (
     <div className={styles.loginBoxWrapper}>
