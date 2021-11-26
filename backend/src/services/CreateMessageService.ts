e724d2cac04b6701fa68fe05818c5484c2f34516
Diego Luis <contato.diegoluisdasilva@gmail.com>
2021-11-26 16:22:56 -0300

    Mudando o retorno de nova mensagem pelo socket

--- backend/src/services/CreateMessageService.ts
+++ backend/src/services/CreateMessageService.ts
@@ -13,17 +13,7 @@ class CreateMessageService {
       },
     });
 
-    const infoWS = {
-      text: message.text,
-      user_id: message.user_id,
-      created_at: message.created_at,
-      user: {
-        name: message.user.name,
-        avatar_url: message.user.avatar_url,
-      },
-    };
-
-    io.emit("new_message", infoWS);
+    io.emit("new_message", message);
 
     return message;
   }
