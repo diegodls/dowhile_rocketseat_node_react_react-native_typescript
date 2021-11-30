084e4d84b20bc15e067cee0765467751bd5ebcb8
Diego Luis <contato.diegoluisdasilva@gmail.com>
2021-11-30 16:47:34 -0300

    Adição estilo mensagem lista vazia

--- mobile/src/components/MessageList/styles.ts
+++ mobile/src/components/MessageList/styles.ts
@@ -1,12 +1,33 @@
 import { StyleSheet } from 'react-native';
+import { COLORS, FONTS } from '../../theme';
 
 export const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  scrollViewContainer: {
     flex: 1,
     paddingHorizontal: 20,
   },
   content: {
     paddingTop: 135,
     paddingBottom: 184,
+  },
+  noMessagesContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  noMessagesTitle: {
+    fontSize: 22,
+    fontFamily: FONTS.BOLD,
+    color: COLORS.GRAY_TERTIARY,
+    marginBottom: 20,
+  },
+  noMessagesSubTitle: {
+    fontSize: 18,
+    fontFamily: FONTS.BOLD,
+    color: COLORS.GRAY_SECONDARY,
+    marginBottom: 20,
   },
 });
